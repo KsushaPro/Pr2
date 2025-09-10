e889ecd172d6e68966f527b4f9b68388d420fe30
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-09-10 20:47:55 +0300

    task1. Поиск минимального элемента

--- src/main/java/org/example/service/Maxmin.java
+++ src/main/java/org/example/service/Maxmin.java
@@ -3,7 +3,7 @@ package org.example.service;
 public class Maxmin {
     public static void main(String[] args) {
         int[] array = new int[5]; // вариант 1 создания массива 5 чисел
-        int[] array1 = new int[]{5, 6, 7, 12, 1, 6, 8, 25, 15}; // вариант 2 создания массива 5 чисел с инициализацией
+        int[] array1 = new int[]{5, 6, 7, 12, -1, 6, 8, 25, 15}; // вариант 2 создания массива 5 чисел с инициализацией
 
         for (int i = 0; i < array1.length; i++) {
             System.out.println((i+1) + "-й элемент = " + array1[i]);
@@ -17,6 +17,12 @@ public class Maxmin {
         }
         System.out.println("Максимальный элемень  массива = " + max);
 
-
+        int min = array1[0];
+        for (int i = 0; i < array1.length; i++) {
+            if (min > array1[i]) {
+                min = array1[i];
+            }
+        }
+        System.out.println("Минимальный элемень  массива = " + min);
     }
 }
