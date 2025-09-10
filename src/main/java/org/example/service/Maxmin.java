19cc06be2d697c46306e5e9cce30d3aeb69ae1c6
Alex Turaev <a.turaev@r77.center-inform.ru>
2025-09-10 20:35:33 +0300

    task1. Поиск максимального элемента

---
+++ src/main/java/org/example/service/Maxmin.java
@@ -0,0 +1,22 @@
+package org.example.service;
+
+public class Maxmin {
+    public static void main(String[] args) {
+        int[] array = new int[5]; // вариант 1 создания массива 5 чисел
+        int[] array1 = new int[]{5, 6, 7, 12, 1, 6, 8, 25, 15}; // вариант 2 создания массива 5 чисел с инициализацией
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println((i+1) + "-й элемент = " + array1[i]);
+        }
+
+        int max = array1[0];
+        for (int i = 0; i < array1.length; i++) {
+            if (max < array1[i]) {
+                max = array1[i];
+            }
+        }
+        System.out.println("Максимальный элемень  массива = " + max);
+
+
+    }
+}
